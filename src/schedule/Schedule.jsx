4185f595a18abb717d9f760ef69b55bbb62d11a0
Cooper Edmunds <cooper6789@gmail.com>
2018-09-21 16:57:37 -0400

    Remove comments

--- src/schedule/Schedule.jsx
+++ src/schedule/Schedule.jsx
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import styled from 'react-emotion';
-//
-// import { SectionBase, SectionTitle } from '../common-styles';
-//
-// import ScheduleLine from './schedule-line.svg';
-//
-// const Container = styled('div')`
-//     ${SectionBase({ height: '800px' })}
-//     background-image: url(${ScheduleLine});
-//     padding-top: 90px;
-// `;
-//
-// const Title = styled('div')`
-//     ${SectionTitle};
-//     color: #a94a64;
-// `;
-//
-// export default () => (
-//     <Container>
-//         <Title>Schedule</Title>
-//     </Container>
-// );
-
 import React from 'react';
 import styled from 'react-emotion';
 
